4ee20ec470b18c452932aa50ec7831c79f1dc4e8
Moonsungjeong <gmsj159@gmail.com>
2022-11-28 15:42:21 +1000

    made First page

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Home from "./Home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Home></Home>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
