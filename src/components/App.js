d87e82d1077747dec30fd63eb298a13b5c748eff
Moonsungjeong <gmsj159@gmail.com>
2022-12-01 22:14:53 +1000

    Made Templates

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+
 import Home from "./Home/Home";
+import Signin from "./Signin/Signin";
+import Signup from "./Signup/Signup";
+import Profile from "./Profile/Profile";
+import Settings from "./Settings/Settings";
+import Editor from "./Editor/Editor";
+import Article from "./Article/Article";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Home></Home>
+      <Profile></Profile>
       <Footer></Footer>
     </div>
   );
