a5d6d3e092efd02df08f8691ff450cfd660b341a
Moonsungjeong <gmsj159@gmail.com>
2022-12-02 22:24:43 +1000

    doing react-router

--- src/components/App.js
+++ src/components/App.js
@@ -9,11 +9,15 @@ import Settings from "./Settings/Settings";
 import Editor from "./Editor/Editor";
 import Article from "./Article/Article";
 
+import { Routes, HashRouter, Route, Link } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Profile></Profile>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer></Footer>
     </div>
   );
